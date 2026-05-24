9906998ac80e3f7e1d56a16c2c875706b58ce41d
Kelvyn <kelvynkam@gmail.com>
2026-05-24 13:37:02 -0300

    0.1.4 - Adicionando mais titulos para estudo de generos

--- js/animes-data.js
+++ js/animes-data.js
@@ -85,6 +85,231 @@ const animes = [
             }
 
         ]
+    },
+
+    {
+        id: "naruto",
+        titulo: "Naruto",
+        generos: "Ação • Shounen",
+        descricao: "Naruto sonha em se tornar Hokage.",
+        sinopse: "Naruto Uzumaki é um jovem ninja rejeitado pela vila por carregar dentro de si a Raposa de Nove Caudas. Mesmo sozinho, ele sonha em se tornar Hokage, o líder mais forte da Vila da Folha.",
+        imagem: "assets/img/capas/naruto.jpg",
+        banner: "assets/img/banners/naruto-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Entre! Naruto Uzumaki!"
+            },
+            {
+                numero: 2,
+                titulo: "Meu Nome é Konohamaru!"
+            },
+            {
+                numero: 3,
+                titulo: "Sasuke e Sakura: Amigos ou Inimigos?"
+            }
+        ]
+    },
+
+    {
+        id: "aot",
+        titulo: "Attack on Titan",
+        generos: "Ação • Drama",
+        descricao: "Humanidade luta contra Titãs gigantes.",
+        sinopse: "Após a destruição de sua cidade por Titãs, Eren Yeager promete exterminar todas as criaturas gigantes que ameaçam a humanidade.",
+        imagem: "assets/img/capas/aot.png",
+        banner: "assets/img/banners/aot-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Para Você, em 2000 Anos"
+            },
+            {
+                numero: 2,
+                titulo: "Naquele Dia"
+            },
+            {
+                numero: 3,
+                titulo: "Uma Luz Fraca em Meio ao Desespero"
+            }
+        ]
+    },
+
+    {
+        id: "demonslayer",
+        titulo: "Demon Slayer",
+        generos: "Ação • Sobrenatural",
+        descricao: "Tanjiro enfrenta demônios para salvar sua irmã.",
+        sinopse: "Após sua família ser massacrada por demônios, Tanjiro Kamado entra para a organização dos Caçadores de Demônios em busca de uma cura para sua irmã Nezuko.",
+        imagem: "assets/img/capas/demonslayer.jpg",
+        banner: "assets/img/banners/demonslayer-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Crueldade"
+            },
+            {
+                numero: 2,
+                titulo: "Treinador Sakonji Urokodaki"
+            },
+            {
+                numero: 3,
+                titulo: "Sabito e Makomo"
+            }
+        ]
+    },
+
+    {   
+        id: "jujutsukaisen",
+        titulo: "Jujutsu Kaisen",
+        generos: "Ação • Sobrenatural",
+        descricao: "Itadori entra no mundo das maldições.",
+        sinopse: "Yuji Itadori engole um objeto amaldiçoado e se torna hospedeiro de Sukuna, entrando para a escola de feiticeiros Jujutsu.",
+        imagem: "assets/img/capas/jjk.jpg",
+        banner: "assets/img/banners/jjk-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Ryomen Sukuna"
+            },
+            {
+                numero: 2,
+                titulo: "Por Mim Mesmo"
+            },
+            {
+                numero: 3,
+                titulo: "Garota de Aço"
+            }
+        ]
+    },
+
+    {
+        id: "deathnote",
+        titulo: "Death Note",
+        generos: "Suspense • Sobrenatural",
+        descricao: "Light encontra um caderno mortal.",
+        sinopse: "Light Yagami encontra o Death Note, um caderno capaz de matar qualquer pessoa cujo nome seja escrito nele.",
+        imagem: "assets/img/capas/deathnote.jpg",
+        banner: "assets/img/banners/deathnote-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Renascimento"
+            },
+            {
+                numero: 2,
+                titulo: "Confronto"
+            },
+            {
+                numero: 3,
+                titulo: "Negociação"
+            }
+        ]
+    },
+
+    {
+        id: "dragonballz",
+        titulo: "Dragon Ball Z",
+        generos: "Ação • Shounen",
+        descricao: "Goku protege a Terra de grandes ameaças.",
+        sinopse: "Goku descobre ser um Saiyajin e enfrenta inimigos cada vez mais poderosos para proteger o planeta Terra.",
+        imagem: "assets/img/capas/dbz.jpg",
+        banner: "assets/img/banners/dbz-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "O Segredo dos Saiyajins"
+            },
+            {
+                numero: 2,
+                titulo: "O Guerreiro Mais Forte da História"
+            },
+            {
+                numero: 3,
+                titulo: "A Combinação Mais Forte do Mundo"
+            }
+        ]
+    },
+
+    {
+        id: "tokyoghoul",
+        titulo: "Tokyo Ghoul",
+        generos: "Terror • Sobrenatural",
+        descricao: "Kaneki se torna meio ghoul.",
+        sinopse: "Após um acidente, Ken Kaneki recebe órgãos de um ghoul e passa a viver entre humanos e criaturas devoradoras de carne.",
+        imagem: "assets/img/capas/tokyoghoul.jpg",
+        banner: "assets/img/banners/tokyoghoul-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Tragédia"
+            },
+            {
+                numero: 2,
+                titulo: "Incubação"
+            },
+            {
+                numero: 3,
+                titulo: "Pomba"
+            }
+        ]
+    },
+
+    {
+        id: "chainsawman",
+        titulo: "Chainsaw Man",
+        generos: "Ação • Terror",
+        descricao: "Denji caça demônios com motosserras.",
+        sinopse: "Denji faz um pacto com Pochita e se transforma no Chainsaw Man para sobreviver em um mundo dominado por demônios.",
+        imagem: "assets/img/capas/chainsawman.jpg",
+        banner: "assets/img/banners/chainsawman-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Cachorro e Motosserra"
+            },
+            {
+                numero: 2,
+                titulo: "Chegada em Tóquio"
+            },
+            {
+                numero: 3,
+                titulo: "Onde Está o Meowy?"
+            }
+        ]
+    },
+
+    {
+        id: "spyxfamily",
+        titulo: "Spy x Family",
+        generos: "Comédia • Ação",
+        descricao: "Uma família falsa cheia de segredos.",
+        sinopse: "O espião Twilight monta uma família falsa sem saber que sua filha é telepata e sua esposa uma assassina.",
+        imagem: "assets/img/capas/spyxfamily.jpg",
+        banner: "assets/img/banners/spyxfamily-banner.jpg",
+
+        episodios: [
+            {
+                numero: 1,
+                titulo: "Operação Strix"
+            },
+            {
+                numero: 2,
+                titulo: "Arrumar uma Esposa"
+            },
+            {
+                numero: 3,
+                titulo: "Preparando-se para a Entrevista"
+            }
+        ]
     }
 
 ];
