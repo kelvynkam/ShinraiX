caabce2611370da1f1a4592ca59f182f0976cda2
Kelvyn <kelvynkam@gmail.com>
2026-06-10 21:53:54 -0300

    v0.2.1 - Estrutura do player

--- js/player.js
+++ js/player.js
@@ -1,16 +1,95 @@
-const params = new URLSearchParams(window.location.search);
+function iniciarPlayer(){
 
-const animeId = params.get("anime");
-const episodioNumero = Number(params.get("ep"));
+    const params = new URLSearchParams(window.location.search);
 
-const anime = animes.find(item => item.id === animeId);
+    const animeId = params.get("anime");
+    const episodioNumero = Number(params.get("ep"));
 
-const episodio = anime.episodios.find(ep => ep.numero === episodioNumero);
+    const anime = animes.find(item => item.id === animeId);
 
-const tituloEpisodio = document.getElementById("titulo-episodio");
-const video = document.querySelector("video");
+    if (!anime) {
+        window.location.href = "../index.html";
+        return;
+    }
 
-tituloEpisodio.textContent =
-`${anime.titulo} - EP ${episodio.numero}: ${episodio.titulo}`;
+    const episodio = anime.episodios.find(ep => ep.numero === episodioNumero);
 
-video.src = "../" + episodio.video;
+    if (!episodio) {
+        window.location.href = `anime.html?anime=${anime.id}`;
+        return;
+    }
+
+    const btnAnterior = document.getElementById("btn-anterior");
+    const btnProximo = document.getElementById("btn-proximo");
+
+    const tituloAnime =
+    document.getElementById("anime-titulo");
+
+    const tituloEpisodio =
+    document.getElementById("episodio-titulo");
+
+    const video =
+    document.querySelector("video");
+
+    tituloAnime.textContent =
+    anime.titulo;
+
+    tituloEpisodio.textContent =
+    `EP ${episodio.numero} - ${episodio.titulo}`;
+
+    video.src = "../" + episodio.video;
+
+    const existeProximo = 
+    anime.episodios.some(
+        ep => ep.numero === episodio.numero + 1
+    );
+
+    const existeAnterior =
+    anime.episodios.some(
+        ep => ep.numero === episodio.numero - 1
+    );
+
+    if(existeAnterior){
+
+        btnAnterior.addEventListener("click", () => {
+
+            const anteriorNumero = episodio.numero - 1;
+
+            window.location.href = 
+            `player.html?anime=${anime.id}&ep=${anteriorNumero}`;
+        });
+
+    } else {
+
+        btnAnterior.disabled = true;
+
+    }
+
+    if(existeProximo){
+
+        btnProximo.addEventListener("click", () => {
+        
+            const proximoNumero = episodio.numero + 1;
+
+            window.location.href = 
+            `player.html?anime=${anime.id}&ep=${proximoNumero}`;
+        });
+
+    } else {
+
+        btnProximo.disabled = true;
+
+    }
+
+    const voltarAnime =
+    document.getElementById("voltar-anime");
+
+    voltarAnime.href =
+    `anime.html?anime=${anime.id}`;
+
+    voltarAnime.textContent =
+    `← Voltar para ${anime.titulo}`;
+
+}
+
+iniciarPlayer();
